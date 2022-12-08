987c50ebf2aef9bfcc721e141f33c12f1e1c1c4c
Konrad <konradpstrongowski@gmail.com>
2022-12-08 18:59:44 +0100

    Day 2 commit

--- src/Day2_RandomPresent.java
+++ src/Day2_RandomPresent.java
@@ -1,5 +1,4 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Day2_RandomPresent {
     /**
@@ -11,6 +10,48 @@ public class Day2_RandomPresent {
      * Wylosowane prezenty wypisz na ekranie. Zadbaj o to, żeby wylosowane prezenty były unikalne.
      */
     public static void main(String[] args) {
-        Set <String> presents = new HashSet<>();
+        List<String> gifts = new ArrayList<>();
+        gifts.add("Hot Wheels car");
+        gifts.add("Barbie doll");
+        gifts.add("Adidas Teamgeist ball");
+        gifts.add("FIFA 23");
+        gifts.add("Xbox Series X");
+        gifts.add("PS5");
+        gifts.add("Iphone 14");
+        gifts.add("Nintendo Switch");
+        gifts.add("Roland drumset");
+        gifts.add("Electric guitar Ibanez");
+
+        Set<String> endGiftsList = new HashSet<>();
+
+
+        // Program uruchomiony 3 razy, zeby potwierdzic ze prezenty przy kolejnym uruchomieniu sie nie powtórzą
+        executedGiftList(gifts, endGiftsList);
+        executedGiftList(gifts, endGiftsList);
+        executedGiftList(gifts, endGiftsList);
+
+
+        for (String gift : endGiftsList) {
+            System.out.println(gift.toString());
+        }
+    }
+
+    public static String drawRandomGift(List<String> giftsList) {
+        Random rand = new Random();
+        String randomElement = giftsList.get((rand.nextInt(giftsList.size())));
+        return randomElement;
+    }
+
+    public static void executedGiftList(List<String> giftsList, Set<String> endList) {
+        String tempGift;
+        int count = 0;
+        while (count < 3) {
+            tempGift = drawRandomGift(giftsList);
+            if (!endList.contains(tempGift)) {
+                endList.add(tempGift);
+                count++;
+            }
+        }
+
     }
 }
